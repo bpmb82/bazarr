a66295eb19db9994664aa0f3b4dbe8dc8e6a4174
bpmb82 <56978424+bpmb82@users.noreply.github.com>
2020-12-30 00:07:24 +0100

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-FILE=/config/config.ini
-
-until test -f $FILE; do sleep 1; done
-
-API=$(cat /config/config.ini | grep 'apikey =' | awk -F '= ' '{print $2}')
-curl -f "http://localhost:6767/sabnzbd/api?mode=queue&output=json&apikey=$API" || exit 1
+curl localhost:6767/api || exit 1
